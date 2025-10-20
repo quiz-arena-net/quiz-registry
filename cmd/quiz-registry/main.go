8d0696a30e1b22e4cff43118bf63e8f4b3737b53
sglre6355 <sglre6355@gmail.com>
2025-10-20 07:22:51 +0100

    Initial commit

---
+++ cmd/quiz-registry/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+	"os"
+
+	"connectrpc.com/grpchealth"
+	"connectrpc.com/grpcreflect"
+	"github.com/quiz-arena-net/quiz-registry/gen/quiz_arena/quiz_registry/v1/quiz_registryv1connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle(grpchealth.NewHandler(
+		grpchealth.NewStaticChecker(
+			quiz_registryv1connect.QuizRegistryServiceName,
+		),
+	))
+	mux.Handle(grpcreflect.NewHandlerV1(
+		grpcreflect.NewStaticReflector(
+			grpchealth.HealthV1ServiceName,
+			quiz_registryv1connect.QuizRegistryServiceName,
+		),
+	))
+	mux.Handle(grpcreflect.NewHandlerV1Alpha(
+		grpcreflect.NewStaticReflector(
+			grpchealth.HealthV1ServiceName,
+			quiz_registryv1connect.QuizRegistryServiceName,
+		),
+	))
+
+	addr := ":50051"
+
+	slog.Info("starting server", "addr", addr)
+	err := http.ListenAndServe(
+		addr,
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+	if err != nil {
+		slog.Error("listen and serve failed", "err", err)
+		os.Exit(1)
+	}
+}
