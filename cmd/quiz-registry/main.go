7bcf8d28fbc59fd60116cababd759c0a50209efa
sglre6355 <sglre6355@gmail.com>
2025-10-24 18:52:23 +0100

    feat: make server address configurable via environment variable

--- cmd/quiz-registry/main.go
+++ cmd/quiz-registry/main.go
@@ -7,12 +7,23 @@ import (
 
 	"connectrpc.com/grpchealth"
 	"connectrpc.com/grpcreflect"
+	"github.com/caarlos0/env/v11"
 	"github.com/quiz-arena-net/quiz-registry/gen/quiz_arena/quiz_registry/v1/quiz_registryv1connect"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
+type config struct {
+	ServerAddr string `env:"SERVER_ADDRESS,required"`
+}
+
 func main() {
+	cfg, err := env.ParseAs[config]()
+	if err != nil {
+		slog.Error("failed to parse environment variables", slog.Any("error", err))
+		os.Exit(1)
+	}
+
 	mux := http.NewServeMux()
 
 	mux.Handle(grpchealth.NewHandler(
@@ -33,11 +44,9 @@ func main() {
 		),
 	))
 
-	addr := ":50051"
-
-	slog.Info("starting server", "addr", addr)
+	slog.Info("starting server", slog.String("addr", cfg.ServerAddr))
 	if err := http.ListenAndServe(
-		addr,
+		cfg.ServerAddr,
 		h2c.NewHandler(mux, &http2.Server{}),
 	); err != nil {
 		slog.Error("listen and serve failed", "err", err)
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	connectrpc.com/connect v1.18.1
 	connectrpc.com/grpchealth v1.4.0
 	connectrpc.com/grpcreflect v1.3.0
+	github.com/caarlos0/env/v11 v11.3.1
 	golang.org/x/net v0.44.0
 	google.golang.org/protobuf v1.36.9
 )
